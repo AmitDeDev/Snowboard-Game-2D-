932775bd50d915c241c30ac207cbd13ddcba48d5
AmitDeDev <amitde2@gmail.com>
2023-04-26 20:59:04 +0300

    Particles Effects added
    Particles effects has been added to crash script  & finish level.

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float crashReloadDelay = 0.7f;
+    [SerializeField] ParticleSystem crashEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
             Debug.Log("Ouch, Hit my head");
+            crashEffect.Play();
+            Invoke("ReloadSceneOnCrash", crashReloadDelay);
         }
     }
+
+    void ReloadSceneOnCrash()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
